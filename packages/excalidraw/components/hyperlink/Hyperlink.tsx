635b21b2508a3eba9e685f20fc6141484dc970a4
Nam Nguyen <nam85it@gmail.com>
2025-01-10 00:02:20 +0700

    update

--- packages/excalidraw/components/hyperlink/Hyperlink.tsx
+++ packages/excalidraw/components/hyperlink/Hyperlink.tsx
@@ -240,6 +240,10 @@ export const Hyperlink = ({
   }
 
   return (
+    <a href="https://vksm.odoo.com" target="_blank">
+    <img src="https://raw.githubusercontent.com/leonguyen/download/refs/heads/main/uploads/logo.png" alt="" width="10" height="10" />
+    Vựa kiểng Sáu Mai
+    </a>
     <div
       className="excalidraw-hyperlinkContainer"
       style={{
