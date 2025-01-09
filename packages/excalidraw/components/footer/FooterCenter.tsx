154c94b3b3a70889bc77cf74db2abbff1e984ec1
Nam Nguyen <nam85it@gmail.com>
2025-01-10 00:02:49 +0700

    update

--- packages/excalidraw/components/footer/FooterCenter.tsx
+++ packages/excalidraw/components/footer/FooterCenter.tsx
@@ -16,13 +16,6 @@ const FooterCenter = ({ children }: { children?: React.ReactNode }) => {
       >
         {children}
       </div>
-      <a href="https://vksm.odoo.com" target="_blank">
-      
-          <img src="https://raw.githubusercontent.com/leonguyen/download/refs/heads/main/uploads/logo.png" alt="" width="10" height="10" />
-          
-              Vựa kiểng Sáu Mai
-              
-                  </a>
     </FooterCenterTunnel.In>
 
   );
